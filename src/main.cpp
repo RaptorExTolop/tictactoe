52cd2badfe1187ef37393676e0487c20cc4ac56f
RaptorExTolop <jamesappletonret@gmail.com>
2025-03-16 11:04:04 +1030

    Restarted project to properly build functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,68 +2,43 @@
 #include <vector>
 
 using namespace std;
-vector<vector<char>> initGame(int width, int height);
-void printGame(vector<vector<char>> g);
-int runGame(vector<vector<char>> gameBoard);
+void makeGameGrid();
+int changeGameGrid(vector<int> v2);
+
+vector<vector<char>> game;
 
 int main() {
-    vector<vector<char>> gameBoard;
-    gameBoard = initGame(3, 3);
-    runGame(gameBoard);
+    makeGameGrid();
     return 0;
 }
 
-vector<vector<char>> initGame(int width, int height) {
-    vector<vector<char>> game;
-    for (int i = 0; i < width; i++) {
+void makeGameGrid() {
+    vector<vector<char>> grid;
+    for (int i = 0; i < 3; i++) {
         vector<char> v;
-        for (int j = 0; j < height; j++) {
+        for (int j = 0; j < 3; j++) {
             v.push_back(' ');
         }
-        game.push_back(v);
-
+        grid.push_back(v);
     }
-    return game;
+    game = grid;
 }
 
-void printGame(vector<vector<char>> g) {
-    for (int i = 0; i < g.size(); i++) {
-        for (int j = 0; j < g[i].size(); j++) {
-            cout << g[i][j];
-        }
-        cout << endl;
+int changeGameGrid(vector<int> v2, bool isPlayerOne) {
+    if (v2.size() != 2) {
+        return 1; // exit code 1 means invalid vector length
     }
-}
-
-int runGame(vector<vector<char>> gameBoard) {
-    cout << "Hello, World!" << endl <<
-    "Welcome to a console based tic-tac-toe game!" << endl << 
-    "You either enter some coordinates (x, y), or type 'q' to quit the game." << endl;
-    
-    string user1name;
-    string user2name;
-    cout << "Enter user 1 name: ";
-    cin >> user1name;
-    cout << endl << "Enter user 2 name: ";
-    cin >> user2name;
-    cout << endl;
-
-    bool running = true;
-    bool p1turn = true;
-
-    while (running) {
-        if (p1turn) {
-            cout << "Player One ";
-        } else {
-            cout << "Player Two";
-        }
-        cout << "Please either quit the game 'q' or enter coordinates 'x, y'" << endl;
-        string input;
-        cin >> input;
-        if (input[0] == 'q') {
-            cout << "quits the game" << endl;
-            running = false;
-        }
+    if (v2[0] > 2 || v2[1] > 2 || v2[0] < 0 || v2[1] < 0) {
+        return 2; // exit code 2 means value/s are larger than the game board
     }
-    return 0;
+    if (game[v2[0]][v2[1]] != ' ') {
+        return 3; // already taken
+    }
+
+    char ch;
+    if (isPlayerOne) {
+        ch = 'X';
+    } else {
+        ch = 'Y'
+    }
 }
